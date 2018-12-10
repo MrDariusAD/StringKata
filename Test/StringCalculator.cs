15dae1f5eab8cd7c9ab48a141c1f893d496ca14b
Salih-Can Selvi <Salih-Can.Selvi@eckd.de>
2018-12-10 09:52:59 +0100

    Add project files.

---
+++ Test/StringCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test {
+    public class StringCalculator {
+        public int Add(string numbers) {
+            var result = 0;
+            if (numbers == "") return 0;
+            var splitChars = new List<string>() { ",", "\n" };
+            var negatives = new List<int>();
+
+            if (numbers.StartsWith("//") && numbers[2] != '[') {
+                splitChars.Add(numbers[2].ToString());
+                numbers = numbers.Substring(4);
+            }
+
+            if (numbers.StartsWith("//") && numbers[2] == '[') {
+                var start = numbers.IndexOf('[') + 1;
+                var end = numbers.IndexOf(']');
+                var length = end - start;
+                splitChars.Add(numbers.Substring(start, length));
+                numbers = numbers.Substring(end + 1);
+            }
+
+            while (numbers.First() == '[') {
+                var start = numbers.IndexOf('[') + 1;
+                var end = numbers.IndexOf(']');
+                var length = end - start;
+                splitChars.Add(numbers.Substring(start, length));
+                numbers = numbers.Substring(end + 1);
+            }
+
+            foreach (var number in numbers.Split(splitChars.ToArray(), StringSplitOptions.RemoveEmptyEntries)) {
+                if (number.StartsWith("-") && !splitChars.Contains("-")) negatives.Add(int.Parse(number));
+                if (!string.IsNullOrWhiteSpace(number) && int.Parse(number) < 1000) result += int.Parse(number);
+            }
+
+            if (negatives.Count != 0) throw new Exception($"Negative Zahlen werden nicht unterstützt. Negative Zahlen = {string.Join(", ", negatives)}");
+
+            return result;
+        }
+    }
+}
---
+++ Test/StringCalculatorTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Test {
+    public class StringCalculatorTests {
+
+        // ZuTestendeMethode_Testfallbeschreibung_Ergebnis
+        [Fact]
+        public void Add_Leerstring_wird_übergeben_0_ist_das_Ergebnis() {
+            // Arrange
+            string numbers = "";
+            var calculator = new StringCalculator();
+            
+            // Act
+            var ergebnis = calculator.Add(numbers);
+            
+            // Assert
+            Assert.Equal(0, ergebnis);
+            ergebnis.Should().Be(0);
+
+        }
+
+        [Fact]
+        public void Add_5_wird_übergeben_5_ist_das_Ergebnis() {
+            // Arrange
+            string numbers = "5";
+            var calculator = new StringCalculator();
+
+            // Act
+            var ergebnis = calculator.Add(numbers);
+
+            // Assert
+            ergebnis.Should().Be(5);
+        }
+
+
+        [Fact]
+        public void Add_Zahlen_werden_mit_kommas_getrennt_übergeben_Summe_der_Zahlen_ist_das_Ergebnis() {
+            // Arrange
+            string numbers = "5,9,8";
+            var calculator = new StringCalculator();
+
+            // Act
+            var ergebnis = calculator.Add(numbers);
+
+            // Assert
+            ergebnis.Should().Be(22);
+        }
+
+        [Fact]
+        public void Add_Zahlen_werden_mit_absätzen_und_kommas_getrennt_übergeben_Summe_der_Zahlen_ist_das_Ergebnis() {
+            // Arrange
+            string numbers = "5\n9,8";
+            var calculator = new StringCalculator();
+
+            // Act
+            var ergebnis = calculator.Add(numbers);
+
+            // Assert
+            ergebnis.Should().Be(22);
+        }
+
+        [Fact]
+        public void Add_Negative_Zahlen_werden_übergeben_es_wird_eine_exception_mit_den_negativen_Zahlen_geworfen() {
+            // Arrange
+            string numbers = "//?\n-5\n-9?8";
+            var calculator = new StringCalculator();
+
+            // Act
+            Action action = () => calculator.Add(numbers);
+            
+            // Assert
+            action.Should().Throw<Exception>();
+
+        }
+
+        [Fact]
+        public void Add_Zahl_über_1000_wird_übergeben_Zahlen_über_1000_werden_ignoriert() {
+            // Arrange
+            string numbers = "10,2,10000";
+            var calculator = new StringCalculator();
+
+            // Act
+            int ergebnis = calculator.Add(numbers);
+
+            // Assert
+            ergebnis.Should().Be(12);
+        }
+
+        [Fact]
+        public void Add_Trennzeichen_mit_mehreren_Stellen_wird_übergeben_Summe_der_Zahlen_ist_das_Ergebnis() {
+            // Arrange
+            string numbers = "//[***]10,2***123";
+            var calculator = new StringCalculator();
+
+            // Act
+            int ergebnis = calculator.Add(numbers);
+
+            // Assert
+            ergebnis.Should().Be(135);
+        }
+        [Fact]
+        public void Add_mehrere_Trennzeichen_mit_mehreren_Stellen_werden_übergeben_Summe_der_Zahlen_ist_das_Ergebnis() {
+            // Arrange
+            string numbers = "//[***][%%][§]10§2***123%%5";
+            var calculator = new StringCalculator();
+
+            // Act
+            int ergebnis = calculator.Add(numbers);
+
+            // Assert
+            ergebnis.Should().Be(140);
+        }
+
+    }
+}
